6bd57fe1f0a2db61e405b7a2c8d70f10cb6251ca
Matt <matt_burgoyne@live.co.uk>
2018-08-20 08:42:23 +0100

    alert on click

---
+++ app.js
@@ -0,0 +1,11 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            value: ''
+        },
+        methods: {
+            showAlert: function() {
+              alert("Button was clicked!");
+            }
+        }
+    });
