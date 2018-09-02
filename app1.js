7691ea9cbb3860e0de2af1cb384c2edbf29ed515
mattb20 <matt_burgoyne@live.co.uk>
2018-09-02 15:18:53 +0100

    looping assignment complete

--- app1.js
+++ app1.js
@@ -2,11 +2,14 @@ new Vue({
   el: '#exercise',
   data: {
     array: ['Max', 'Anna', 'Chris', 'Manu'],
-    myObject: {
-      title: 'Lord of the Rings',
-      author: 'J.R.R. Tolkiens',
-      books: '3',
-    },
+    players: [
+      {name: 'Mohamed Salah', team: 'Liverpool FC', goals: 32},
+      {name: 'Harry Kane', team: 'Tottenham', goals: 30},
+      {name: 'Kun Aguero', team: 21, goals: 18},
+      {name: 'Jamie Vardy', team: 'Leicester City', goals: 20},
+      {name: 'Raheem Sterling', team: 'Manchester City', goals: 18}
+    ],
+    object: {title: 'Lord of the Rings', author: 'J.R.R. Tolkiens', books: '3'},
     show: true,
     testData: {
       name: 'TESTOBJECT',
