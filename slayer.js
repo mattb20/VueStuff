05efa96a0ce05ba2a8e6e228aa35b9ac5e688dc9
mattb20 <matt_burgoyne@live.co.uk>
2018-09-08 17:55:31 +0100

    new game now sets text seen to false, save amount being done as damage and hp gained

--- slayer.js
+++ slayer.js
@@ -4,7 +4,8 @@ new Vue({
     monsterHealth: 100,
     userHealth: 100,
     gameRunning: false,
-    userDamage: '',
+    userDamageOrHeal: '',
+    attackOrHeal: '',
     monsterDamage: '',
     seen: false
   },
@@ -12,20 +13,26 @@ new Vue({
      newGame: function(event) {
          this.monsterHealth = 100;
          this.userHealth = 100;
+         this.seen = false;
   },
   attack: function(event) {
       this.monsterAttack();
       this.monsterHealth -= 10;
       this.seen = true;
-      this.userDamage = 10;
+      this.userDamageOrHeal = 10;
+      this.attackOrHeal = 'attacked';
     },
     heal: function(event) {
-      this.userHealth += Math.floor(Math.random() * 11);
+      let heal = Math.floor(Math.random() * 11);
+      this.userHealth += heal;
+      userDamageOrHeal = heal;
+      this.attackOrHeal = 'healed';
       this.monsterAttack();
     },
     monsterAttack: function() {
-      this.userHealth -= Math.floor(Math.random() * 11);
-      this.monsterDamage = Math.floor(Math.random() * 11);
+      let monsterAttack = Math.floor(Math.random() * 11);
+      this.userHealth -= monsterAttack;
+      this.monsterDamage = monsterAttack;
     },
     specialAttack: function() {
       let attack = Math.floor(Math.random() * 6) * 10;
