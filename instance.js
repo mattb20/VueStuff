70fb40e0ff75a24fe7435489752578a07e21e6ab
mattb20 <matt_burgoyne@live.co.uk>
2018-09-30 15:02:39 +0100

    assign vue objects to variables

---
+++ instance.js
@@ -0,0 +1,38 @@
+var vm1 = new Vue({
+  el: '#app',
+  data: {
+    title: 'The VueJS Instance',
+    showParagraph: false
+  },
+  methods: {
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
+    },
+    updateTitle: function(title) {
+      this.title = title;
+    }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+});
+
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'The second VueJS instance'
+  },
+  methods: {
+    onChange: function() {
+      vm1.title = 'Changed title again';
+    }
+  }
+})
