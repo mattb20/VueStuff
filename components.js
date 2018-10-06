68dbdff4dd045e84d0b3cf6620397bfbd81724cb
mattb20 <matt_burgoyne@live.co.uk>
2018-10-06 13:41:06 +0100

    component attached to two different divs through two vue instances

---
+++ components.js
@@ -0,0 +1,21 @@
+Vue.component('my-cmp', {
+  data: function() {
+    return {
+      status: 'Critical'
+    };
+  },
+  template: '<p>Server Status: {{ status }} (<button @click="changeStatus">Change</button>)</p>',
+  methods: {
+    changeStatus: function() {
+      this.status = 'Normal';
+    }
+  }
+});
+
+new Vue({
+  el: '#app'
+})
+
+new Vue({
+  el: '#app2'
+})
