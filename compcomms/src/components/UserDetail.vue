7c424d9d9202a55c7e56bf8f5cc00b59fdda215b
mattb20 <matt_burgoyne@live.co.uk>
2019-02-14 20:07:30 +0000

    transfer data from parent to child

--- compcomms/src/components/UserDetail.vue
+++ compcomms/src/components/UserDetail.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
+        <h3>You may view the User Details here:</h3>
+        <p>User Name: {{ name }}</p>
     </div>
 </template>
 
 <script>
+  export default {
+    
+  }
 </script>
 
 <style scoped>
