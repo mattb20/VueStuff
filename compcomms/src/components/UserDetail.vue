a93e402890a70a5cbbe9eb98d00598dd8f77eaad
Matt <matt_burgoyne@live.co.uk>
2019-02-18 08:28:13 +0000

    validate props

--- compcomms/src/components/UserDetail.vue
+++ compcomms/src/components/UserDetail.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="component">
         <h3>You may view the User Details here:</h3>
-        <p>User Name: {{ name }}</p>
+        <p>User Name: {{ reverseName() }}</p>
     </div>
 </template>
 
 <script>
   export default {
-    props: ['name', ]
+    props: {
+      name: String
+    },
+    methods: {
+      reverseName() {
+        return this.name.split("").reverse().join("");
+      }
+    }
   }
 </script>
 
