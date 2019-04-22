a7464a0b4cbc3dd5663d94e846aeb239d13bd9be
mattb20 <matt_burgoyne@live.co.uk>
2019-04-22 15:57:48 +0100

    begin server component work

---
+++ componentassignment/src/components/Server/Servers.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li class="list-group-item" v-for="index in 5"> Server #{{ index }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import ServerDetails from './ServerDetails.vue';
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    {id: 1, status: 'Started'},
+                    {id: 2, status: 'Stopped'}
+                ] 
+            };
+        }
+    }
+</script>
+
+<style>
+
+</style>
